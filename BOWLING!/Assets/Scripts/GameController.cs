78d886d8093c48660c33231fba69839e1465bce5
SerebriakovAlex <serebriakovalex2236@gmail.com>
2019-05-15 19:40:30 +0300

    Добавлен экран загрузки

--- BOWLING!/Assets/Scripts/GameController.cs
+++ BOWLING!/Assets/Scripts/GameController.cs
@@ -66,7 +66,7 @@ public class GameController : MonoBehaviour
 
     public void RestartScene()
     {
-        SceneManager.LoadScene("SampleScene");
+        SceneManager.LoadScene("MainScene");
     }
 
     /// <summary>
---
+++ BOWLING!/Assets/Scripts/LoadingControl.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LoadingControl : MonoBehaviour
+{
+    public Image filledProgressBar;
+    AsyncOperation _loadingOperation;
+    private void Start()
+    {
+        _loadingOperation = SceneManager.LoadSceneAsync("MainScene");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        filledProgressBar.fillAmount = _loadingOperation.progress;
+    }
+}
