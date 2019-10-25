087fe92d04abb339f220b114db729abf608d376e
SerebriakovAlex <serebriakovalex2236@gmail.com>
2019-10-25 22:23:05 +0300

    Создано меню для выбора шара

---
+++ BOWLING!/Assets/Scripts/CameraPoint.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraPoint : MonoBehaviour
+{
+    public Quaternion Rotation;
+    public Transform Offset;
+    
+}
